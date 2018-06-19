62eb654ce402bc950fd7808b6dc89b158d0216eb
Arka Sadhu <ark.sadhu2904@gmail.com>
2018-06-19 02:22:33 -0700

    Removing useless files

--- code/cfg.py
+++
@@ -1,9 +0,0 @@
-import json
-import munch
-
-
-def process_config(config_file):
-    with open(config_file, 'r') as f:
-        config_dict = json.load(f)
-    config = munch.Munch(config_dict)
-    return config
--- code/data_loader.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch.utils.data import Dataset
-
-
-class INat_simple(Dataset):
-    def __init__(self, config):
-        super().__init__()
-        self.config = config
